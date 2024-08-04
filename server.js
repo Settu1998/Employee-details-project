47e7632fd9b6ff321dea48b6f885082c0861d0a3
settu1998 <settubsc98@gmail.com>
2024-08-04 20:13:34 +0530

    server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = 5000
 const start = async () =>{
 try{
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listening on port ${port}...`));
+    app.listen(port, console.log(`Server is listening on port ${port}... oik`));
 }
 catch(error){
 console.log(error);
